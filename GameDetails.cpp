6cd5f25a100793255e994efbf6882d95d6517898
Jordan Stanley <jordanstanley_813@hotmail.com>
2017-11-10 11:07:36 -0500

    added cpp file for header file

---
+++ GameDetails.cpp
@@ -0,0 +1,13 @@
+/*
+ * =================================
+ * File: GameDetails.cpp
+ * 
+ * Authors: 
+ * Jordan Stanley (7432058)
+ * jstan064@uottawa.ca
+ * Hussein Sakr (7897706)
+ * hsakr011@uottawa.ca
+ * =================================
+ */
+
+#include "GameDetails.h"
