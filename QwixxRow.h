e960d809927dc9c1b306d0784c41088e6efda48a
Jordan Stanley <jordanstanley_813@hotmail.com>
2017-11-29 21:53:55 -0500

    still working on qwixxrow

--- QwixxRow.h
+++ QwixxRow.h
@@ -22,173 +22,104 @@
  
 template <class T, Colour colour>
 class QwixxRow {
-    T<int> container;
+    T container;
+    string a[11] = {" 2"," 3"," 4"," 5"," 6"," 7"," 8"," 9","10","11","12"};
     public:
 	
-	int& operator+= (*this, const RollOfDice& rollofdice) {
-		string msg = "Your roll has more than 2 dice and can't be used to scored";
-		if (rollofdice.diceVec.size() > 2 ) {
-                    throw length_error(msg);
-		}
-                int rd = static_cast<int>(rollofdice);
-                switch (colour) {
-                    case Colour::RED: case Colour::YELLOW:
-                        vector<int> tmp;
-                        for (auto i : container) {
-                            tmp.push_back(i);
-                        }
-                        if (tmp.back() < rd) {
-                            tmp.push_back(rd);
-                            this->container = tmp;
-                        }
-                    break;
-                    
-                    case Colour::GREEN: case Colour::BLUE:
-                        list<int> tmp;
-                        for (auto i : container) {
-                            tmp.emplace_back(i);
-                        }
-                        if (tmp.back() > rd) {
-                            tmp.emplace_back(rd);
-                            this->container = tmp;
-                        } 
-                    break;
-                    
-                    return *this;
-                }
-                
-                
-                
-	}
-	friend ostream& operator<< (ostream &os, const QwixxRow &obj) {
+	QwixxRow& operator+= (const RollOfDice& rollofdice);
+		
+        
+	friend ostream& operator<< (ostream &os, QwixxRow &obj) {
             string output; 
-            string tmp;
             output = "|";
-            int y = 2;
+            int y = 0;
             int entryCounter = 0;
             
             switch (colour) {
-                case Colour::RED:
+                case Colour::RED: case Colour::YELLOW:
                     entryCounter = 0;
-                    y = 2;
-                    for (int i : obj.container) {
-                       if (i == 0 && y <= 9) {
-                           tmp = " " + to_string(y) + "|";
-                           output += tmp;
-                       } 
-                       if (i == 0 && y > 9) {
-                           tmp = to_string(y) + "|"; 
-                           output += tmp;
-                       }
-                       if (i != 0) {
-                           entryCounter++;
-                           tmp = "XX|";
-                           output += tmp;
-                       }
-                       y++;
+                    y = 0;
+                    for (auto x : obj.container) {
+                        for (y; y < 11; y++) {
+                            if (obj.a[y] == to_string(x)) {
+                                obj.a[y] = "XX";
+                                entryCounter++;
+                            }
+                        }
+                    }
+                    for (int i = 0; i < 11; i++) {
+                        output += obj.a[i] + "|";
                     }
                     if (entryCounter == 5) {
-                        tmp = " L";
-                        output += tmp;
+                        output += " L";
                     }
-                    if (entryCounter != 5) {
-                        tmp = " U";
-                        output += tmp;
+                    else {
+                        output += " U";
                     }
                     os << output;
                 break;
                 
-                case Colour::YELLOW:
+                case Colour::GREEN: case Colour::BLUE:
                     entryCounter = 0;
-                    y = 2;
-                    for (int i : obj.container) {
-                       if (i == 0 && y <= 9) {
-                           tmp = " " + to_string(y) + "|";
-                           output += tmp;
-                       } 
-                       if (i == 0 && y > 9) {
-                           tmp = to_string(y) + "|"; 
-                           output += tmp;
-                       }
-                       if (i != 0) {
-                           entryCounter++;
-                           tmp = "XX|";
-                           output += tmp;
-                       }
-                       y++;
+                    y = 11;
+                    for (auto x : obj.container) {
+                        for (y; y > 0; y--) {
+                            if (obj.a[y] == to_string(x)) {
+                                obj.a[y] = "XX";
+                                entryCounter++;
+                            }
+                        }
+                    }
+                    for (int z = 11; z > 0; z--) {
+                        output += obj.a[z] + "|";
                     }
                     if (entryCounter == 5) {
-                        tmp = " L";
-                        output += tmp;
+                        output += " L";
                     }
-                    if (entryCounter != 5) {
-                        tmp = " U";
-                        output += tmp;
+                    else {
+                        output += " U";
                     }
                     os << output;
-                break;
-                
-                case Colour::GREEN:
-                    entryCounter = 0;
-                    y = 12;
-                    for (int i : obj.container) {
-                       if (i == 0 && y <= 9) {
-                           tmp = " " + to_string(y) + "|";
-                           output += tmp;
-                       } 
-                       if (i == 0 && y > 9) {
-                           tmp = to_string(y) + "|"; 
-                           output += tmp;
-                       }
-                       if (i != 0) {
-                           entryCounter++;
-                           tmp = "XX|";
-                           output += tmp;
-                       }
-                       y--;
-                    }
-                    if (entryCounter == 5) {
-                        tmp = " L";
-                        output += tmp;
-                    }
-                    if (entryCounter != 5) {
-                        tmp = " U";
-                        output += tmp;
-                    }
-                    os << output;
-                break;
-                
-                case Colour::BLUE:
-                    entryCounter = 0;
-                    y = 12;
-                    for (int i : obj.container) {
-                       if (i == 0 && y <= 9) {
-                           tmp = " " + to_string(y) + "|";
-                           output += tmp;
-                       } 
-                       if (i == 0 && y > 9) {
-                           tmp = to_string(y) + "|"; 
-                           output += tmp;
-                       }
-                       if (i != 0) {
-                           entryCounter++;
-                           tmp = "XX|";
-                           output += tmp;
-                       }
-                       y--;
-                    }
-                    if (entryCounter == 5) {
-                        tmp = " L";
-                        output += tmp;
-                    }
-                    if (entryCounter != 5) {
-                        tmp = " U";
-                        output += tmp;
-                    }
-                    os << output;
-                break;                            
+                break; 
              return os;
             }
         }
     };
+    template<class vector, Colour colour>
+    QwixxRow& QwixxRow::operator+= (const RollOfDice& rollofdice) {
+                string msg = "Your roll has more than 2 dice and can't be used to scored";
+		if (rollofdice.diceVec.size() > 2 ) {
+                    throw length_error(msg);
+		}
+                
+                int rd = static_cast<int>(rollofdice);
+                
+                vector<int> tmpVec;
+                
+                switch (colour) {
+                    case Colour::RED: case Colour::YELLOW:
+                        for (auto i : container) {
+                            tmpVec.push_back(i);
+                        }
+                        if (tmpVec.back() < rd) {
+                            tmpVec.push_back(rd);
+                            container = tmpVec;
+                        }
+                    break;
+                    /*
+                    case Colour::GREEN: case Colour::BLUE:
+                        for (auto i : container) {
+                            tmpList.emplace_back(i);
+                        }
+                        if (tmpList.back() > rd) {
+                            tmpList.emplace_back(rd);
+                            container = tmpList;
+                        } 
+                    break;
+                     */
+                }
+                 
+            return *this;           
+    }
 #endif/* QWIXXROW_H */
+    
